04d6ca5d14ebf5c14e85375eb0ed12b490b34215
Elippsis007 <michaeldrmm@gmail.com>
2021-09-01 14:33:51 +0000

    test

--- run.py
+++ run.py
@@ -15,17 +15,17 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", page_title="Greatest Of All Time")
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html")    
+    return render_template("careers.html", page_title="Careers")    
 
 # The word 'main' wrapped in double-underscores (__main__) is the name of the default module in Python.
 if __name__ == "__main__":
