7b9b172a57ebc5cd1fb9be9c4150c2f6eac7bbe6
Elippsis007 <michaeldrmm@gmail.com>
2021-09-02 18:01:53 +0000

    Deployment to Heroku

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 # create an instance of this class
 # The first argument of the Flask class, is the name of the application's module - our package.
 app = Flask(__name__)
-
+app.secret_key = os.environ.get("SECRET_KEY")
 # use the route decorator to tell Flask what URL should trigger the function that follows
 # "pie notation"
 # When we try to browse to the root directory, as indicated by the "/", then Flask triggers the index function underneath and returns the "Hello, World" text.
@@ -33,8 +35,11 @@ def goat_player(player_name):
     return render_template("player.html", player=player)
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
